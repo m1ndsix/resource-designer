06fd4bf9c42df2f69422b7523cdb7ef3a652cb9e
Akhmad Dias <2ahmad86@gmail>
2022-11-07 11:19:07 +0600

    add more data and routes to local demo

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,12 @@
 import { faker } from '@faker-js/faker/locale/ru';
 
-let data = { 'cpr-resource-order-manual': [], 'product-offer-request': null };
+let data = {
+  'cpr-resource-order-manual': [],
+  'product-offer-request': null,
+  'po-req-item': [],
+};
 // Create 20 orders
 for (let i = 0; i < 20; i++) {
-  let randomNumber = faker.random.numeric(6);
   let randomDate = faker.date.between(
     '2020-01-01T00:00:00.000Z',
     '2022-01-01T00:00:00.000Z'
@@ -11,7 +14,7 @@ for (let i = 0; i < 20; i++) {
   let randomName = faker.name.fullName();
   data['cpr-resource-order-manual'].push({
     id: i,
-    externalId: randomNumber,
+    externalId: 123,
     productOfferRequestId: 50000 + i,
     stateId: 1,
     stateData: { id: 1, name: 'Новый' },
@@ -55,7 +58,6 @@ for (let i = 0; i < 20; i++) {
     createApp: 'dev app',
     updateApp: 'dev app',
   });
-
   data['product-offer-request'].push({
     id: 50000 + i,
     divisionId: 123,
@@ -76,6 +78,101 @@ for (let i = 0; i < 20; i++) {
     createApp: randomDate,
     updateApp: randomDate,
   });
+  for (let j = 1; j < 6; j++) {
+    let geoPlaceIdOne = faker.random.numeric(6);
+    let geoPlaceIdTwo = faker.random.numeric(6);
+    let geoPlaceNameOne = faker.address.streetAddress(true);
+    let geoPlaceNameTwo = faker.address.streetAddress(true);
+    let components = [
+      {
+        id: 70000 + j,
+        typeId: j >= 1 && j <= 3 ? j : 3,
+        type: {
+          id: j >= 1 && j <= 3 ? j : 3,
+          nameRu: j === 1 ? 'Установка' : j === 2 ? 'Замена' : 'Снятие',
+          nameKz: j === 1 ? 'Установка' : j === 2 ? 'Замена' : 'Снятие',
+        },
+        poReqItemId: 60000 + j,
+        geoPlaceId: j % 2 === 0 ? geoPlaceIdOne : geoPlaceIdTwo,
+        geoPlaceName: j % 2 === 0 ? geoPlaceNameOne : geoPlaceNameTwo,
+        poComponentId: 1234,
+        poComponentName: 'iD TV с Элитным пакетом каналов дополнительный',
+        oldPoStructId: 1234,
+        oldPoStructName: 'test',
+        newPoStructId: 4321,
+        newPoStructName: 'test',
+        resourceOrderItemId: 1234,
+        oldNumber: '870762239094',
+        newNumber: '877713535433',
+        oldCount: 1234,
+        newCount: 1234,
+        elements: 1234,
+        createDate: randomDate,
+        updateDate: randomDate,
+        createUser: randomDate,
+        updateUser: randomDate,
+        createApp: randomDate,
+        updateApp: randomDate,
+      },
+      {
+        id: 70000 + j,
+        typeId: j >= 1 && j <= 3 ? j : 3,
+        type: {
+          id: j >= 1 && j <= 3 ? j : 3,
+          nameRu: j === 1 ? 'Установка' : j === 2 ? 'Замена' : 'Снятие',
+          nameKz: j === 1 ? 'Установка' : j === 2 ? 'Замена' : 'Снятие',
+        },
+        poReqItemId: 60000 + j,
+        geoPlaceId: j % 2 === 0 ? geoPlaceIdOne : geoPlaceIdTwo,
+        geoPlaceName: j % 2 === 0 ? geoPlaceNameOne : geoPlaceNameTwo,
+        poComponentId: 1234,
+        poComponentName: 'iD TV с Элитным пакетом каналов дополнительный',
+        oldPoStructId: 1234,
+        oldPoStructName: 'test',
+        newPoStructId: 4321,
+        newPoStructName: 'test',
+        resourceOrderItemId: 1234,
+        oldNumber: '870762239094',
+        newNumber: '877713535433',
+        oldCount: 1234,
+        newCount: 1234,
+        elements: 1234,
+        createDate: randomDate,
+        updateDate: randomDate,
+        createUser: randomDate,
+        updateUser: randomDate,
+        createApp: randomDate,
+        updateApp: randomDate,
+      },
+    ];
+
+    data['po-req-item'].push({
+      id: 60000 + j,
+      typeId: j >= 1 && j <= 3 ? j : 3,
+      type: {
+        id: j >= 1 && j <= 3 ? j : 3,
+        nameRu: j === 1 ? 'Установка' : j === 2 ? 'Замена' : 'Снятие',
+        nameKz: j === 1 ? 'Установка' : j === 2 ? 'Замена' : 'Снятие',
+      },
+      productOfferReqId: 50000 + i,
+      geoPlaceId: j % 2 === 0 ? geoPlaceIdOne : geoPlaceIdTwo,
+      geoPlaceName: j % 2 === 0 ? geoPlaceNameOne : geoPlaceNameTwo,
+      oldProductOfferId: 1234,
+      oldProductOfferName: j % 2 === 0 ? 'Silver LTE Prome Kcell(1 год)' : null,
+      newProductOfferId: 4321,
+      newProductOfferName: 'Light LTE Prome Kcell(1 год)',
+      caSubscriptionId: 123,
+      agreementId: 123,
+      billingAccountId: 123,
+      createDate: randomDate,
+      updateDate: randomDate,
+      createUser: randomDate,
+      updateUser: randomDate,
+      createApp: randomDate,
+      updateApp: randomDate,
+      itemComponents: components,
+    });
+  }
 }
 
 console.log(JSON.stringify(data));
