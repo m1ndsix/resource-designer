7720160ad5709941b4826bb092f114ac5ee94815
Lovemetal <sonmkm043@gmail.com>
2023-03-03 16:59:53 +0600

    Some changes to edit item page

--- src/pages/vxEditItem.vue
+++ src/pages/vxEditItem.vue
@@ -1,19 +1,81 @@
 <template>
-  <q-page padding>
-    <VxItemCard v-for="item in itemsExample" :key="item.id" :item="item" />
-  </q-page>
+  <div class="q-pa-sm">
+    <q-table
+      title="Редактирование позиции заказа"
+      :rows="components"
+      :columns="columns"
+      row-key="name"
+      selection="none"
+      :hide-pagination="true"
+    >
+      <template v-slot:body-cell-selection="scope">
+        <q-td>
+          <div class="q-pa-md">
+            <q-checkbox v-model="scope.row.selected" />
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-actions="scope">
+        <q-td>
+          <div class="q-pa-md q-gutter-sm">
+            <q-btn
+              v-model="scope.selected"
+              color="primary"
+              label="Редактировать"
+            />
+
+            <q-btn color="red" label="Отменить" />
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:top>
+        <div class="text-h4">
+          г. Алматы, ул. Абылай хана
+          {{ geoPlaceName() }}
+        </div>
+        <q-space />
+        <div class="q-pa-md">
+          <div class="row q-gutter-sm">
+            <q-btn-group>
+              <q-btn
+                color="primary"
+                glossy
+                label="Измеритель"
+                @click="() => (openMeasurementDialog = true)"
+              ></q-btn>
+            </q-btn-group>
+
+            <q-btn-group>
+              <q-btn color="primary" glossy label="Проверка линии"></q-btn>
+              <q-btn color="primary" outline label="ЛД" disabled>
+                <q-tooltip>
+                  {TOWN:727;STATION:272; MAN:ECI; OLT:4/00/08/01;ODF:2/03/08/13;
+                  LD:ОРШ 272/07: 00/ODF-1/00/29, 00/ODF-5/00/118,
+                  00/ODF-5/00/121, 00/ODF-1/00/138; ОРКсп 272/07/29/3:
+                  00/00/1х8/7; OU:0;ONUPORTNUM:0; (DSL,IPTV,FXS); 6.518 км}
+                </q-tooltip>
+              </q-btn>
+            </q-btn-group>
+          </div>
+        </div>
+      </template>
+    </q-table>
+  </div>
+  <br />
+  <q-dialog v-model="openMeasurementDialog">
+    <vx-measurement />
+  </q-dialog>
 </template>
 
 <script>
 const columns = [
-  { name: 'actions', label: 'Действие', field: 'action', align: 'left' },
   {
-    name: 'position',
-    required: true,
-    label: 'Номер позиции',
-    field: 'position', //rowindex?
-    align: 'center',
+    name: 'selection',
+    label: 'Выбрать',
+    field: (row) => row.selected,
+    align: 'left',
   },
+  { name: 'actions', label: 'Действие', field: 'action', align: 'center' },
   {
     name: 'spec',
     align: 'center',
@@ -56,122 +118,97 @@ const columns = [
     field: 'transportCpeFuncSpecId',
     sortable: true,
   },
-];
-
-const itemsExample = [
   {
-    name: 1,
-    components: [
-      {
-        id: 23654,
-        actionId: 1,
-        actionData: {
-          id: 1,
-          name: 'Установка',
-        },
-        stateId: 2,
-        stateData: {
-          id: 2,
-          name: 'В работе',
-        },
-        externalId: '456gtr65',
-        compositePhysResSpecId: 1000776,
-        physicalContainerId: 354,
-        geoPlaceId: 123,
-        portnumber: 8,
-        transportCpeFuncSpecId: 13,
-        externalProjectId: 1423375,
-        externalCompositePhysResNum: 3425313,
-        processUserId: 543,
-        processDate: '2022-08-24T14:15:22Z',
-        createDate: '2022-06-28T14:15:22Z',
-        updateDate: '2022-06-28T14:15:22Z',
-        createUser: 'Japparov.S',
-        updateUser: 'Japparov.S',
-        createApp: 'resource-design-fe',
-        updateApp: 'resource-design-fe',
-        selected: false,
-        measurementResult: 'Линия в порядке',
-      },
-      {
-        id: 23655,
-        actionId: 1,
-        actionData: {
-          id: 1,
-          name: 'Установка',
-        },
-        stateId: 2,
-        stateData: {
-          id: 2,
-          name: 'В работе',
-        },
-        externalId: '456gtr6513',
-        compositePhysResSpecId: 1000776,
-        physicalContainerId: 355,
-        geoPlaceId: 456,
-        portnumber: 3,
-        transportCpeFuncSpecId: 14,
-        externalProjectId: 1423375,
-        externalCompositePhysResNum: 3425313,
-        processUserId: 543,
-        processDate: '2022-08-24T14:15:22Z',
-        createDate: '2022-06-28T14:15:22Z',
-        updateDate: '2022-06-28T14:15:22Z',
-        createUser: 'Japparov.S',
-        updateUser: 'Japparov.S',
-        createApp: 'resource-design-fe',
-        updateApp: 'resource-design-fe',
-        selected: false,
-        measurementResult: 'Линия в порядке',
-      },
-    ],
+    name: 'measurement-result',
+    align: 'center',
+    label: 'Результат измерения',
+    field: 'measurementResult',
+    sortable: true,
+  },
+];
+const components = [
+  {
+    id: 23654,
+    actionId: 1,
+    actionData: {
+      id: 1,
+      name: 'Установка',
+    },
+    stateId: 2,
+    stateData: {
+      id: 2,
+      name: 'В работе',
+    },
+    externalId: '456gtr65',
+    compositePhysResSpecId: 1000776,
+    physicalContainerId: 354,
+    geoPlaceId: 123,
+    portnumber: 8,
+    transportCpeFuncSpecId: 13,
+    externalProjectId: 1423375,
+    externalCompositePhysResNum: 3425313,
+    processUserId: 543,
+    processDate: '2022-08-24T14:15:22Z',
+    createDate: '2022-06-28T14:15:22Z',
+    updateDate: '2022-06-28T14:15:22Z',
+    createUser: 'Japparov.S',
+    updateUser: 'Japparov.S',
+    createApp: 'resource-design-fe',
+    updateApp: 'resource-design-fe',
+    selected: false,
+    measurementResult: 'Линия в порядке',
   },
   {
-    name: 2,
-    components: [
-      {
-        id: 23655,
-        actionId: 1,
-        actionData: {
-          id: 1,
-          name: 'Замена',
-        },
-        stateId: 2,
-        stateData: {
-          id: 2,
-          name: 'В работе',
-        },
-        externalId: '456gtestr6513',
-        compositePhysResSpecId: 1000776,
-        physicalContainerId: 245,
-        geoPlaceId: 988,
-        portnumber: 3,
-        transportCpeFuncSpecId: 14,
-        externalProjectId: 1423375,
-        externalCompositePhysResNum: 3425313,
-        processUserId: 543,
-        processDate: '2022-08-24T14:15:22Z',
-        createDate: '2022-06-28T14:15:22Z',
-        updateDate: '2022-06-28T14:15:22Z',
-        createUser: 'Japparov.S',
-        updateUser: 'Japparov.S',
-        createApp: 'resource-design-fe',
-        updateApp: 'resource-design-fe',
-        selected: false,
-        measurementResult: 'Линия в порядке',
-      },
-    ],
+    id: 23655,
+    actionId: 1,
+    actionData: {
+      id: 1,
+      name: 'Установка',
+    },
+    stateId: 2,
+    stateData: {
+      id: 2,
+      name: 'В работе',
+    },
+    externalId: '456gtr6513',
+    compositePhysResSpecId: 1000776,
+    physicalContainerId: 355,
+    geoPlaceId: 456,
+    portnumber: 3,
+    transportCpeFuncSpecId: 14,
+    externalProjectId: 1423375,
+    externalCompositePhysResNum: 3425313,
+    processUserId: 543,
+    processDate: '2022-08-24T14:15:22Z',
+    createDate: '2022-06-28T14:15:22Z',
+    updateDate: '2022-06-28T14:15:22Z',
+    createUser: 'Japparov.S',
+    updateUser: 'Japparov.S',
+    createApp: 'resource-design-fe',
+    updateApp: 'resource-design-fe',
+    selected: false,
+    measurementResult: 'Линия в порядке',
   },
 ];
 
-import { defineComponent } from 'vue';
-import VxItemCard from 'src/components/vxItemCard.vue';
+import { ref } from 'vue';
+import { usePrepareStore } from 'stores/prepare';
+//import { useOrderStore } from 'src/stores/order';
+
 export default {
   setup() {
+    const prepareStore = usePrepareStore();
+    //const orderStore = useOrderStore();
     return {
-      itemsExample,
+      columns,
+      components,
+      prepareStore,
     };
   },
-  components: { VxItemCard },
+  methods: {
+    geoPlaceName() {
+      return this.prepareStore.geoPlace;
+    },
+  },
 };
 </script>
