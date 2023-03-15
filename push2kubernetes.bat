f65e941dacd26669c3eef5fc0f76cf39c3a2b082
Lovemetal <sonmkm043@gmail.com>
2023-03-15 14:25:15 +0600

    Kubernetes project name change. Set version to 0.1

--- push2kubernetes.bat
+++ push2kubernetes.bat
@@ -1,3 +1,3 @@
-docker build --tag alma-kube-registry.cdn.telecom.kz/vcip/resource-design-fe:version1.0 . && ^
+docker build --tag alma-kube-registry.cdn.telecom.kz/cpr-resource-order-fe/resource-design-fe:version0.1 . && ^
 docker login alma-kube-registry.cdn.telecom.kz && ^
-docker image push alma-kube-registry.cdn.telecom.kz/vcip/resource-design-fe:version1.0
+docker image push alma-kube-registry.cdn.telecom.kz/cpr-resource-order-fe/resource-design-fe:version0.1
