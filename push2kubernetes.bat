6a27b79e6f0ee337b23aaf8ba2205d0d69ca9165
m1ndsix <vladmalyshev666@gmail.com>
2024-03-28 14:54:23 +0500

    Change kubernetes address

--- push2kubernetes.bat
+++ push2kubernetes.bat
@@ -1,3 +1,3 @@
-docker build --tag alma-kube-registry.cdn.telecom.kz/cpr-resource-order-fe/resource-design-fe:version0.1 . && ^
-docker login alma-kube-registry.cdn.telecom.kz && ^
-docker image push alma-kube-registry.cdn.telecom.kz/cpr-resource-order-fe/resource-design-fe:version0.1
+docker build --tag harbor.telecom.kz/cpr-resource-order-fe/resource-design-fe:version0.2 . && ^
+docker login harbor.telecom.kz && ^
+docker image push harbor.telecom.kz/cpr-resource-order-fe/resource-design-fe:version0.2
